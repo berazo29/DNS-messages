efb2f076abf00ccf4f920168be759e0b899b7b2f
Bryan Erazo <bryanerazo29@gmail.com>
2021-03-23 08:46:52 -0400

    Multiple responses handler implemented

--- Server.py
+++ Server.py
@@ -1,3 +1,10 @@
+""""
+Author: Bryan Erazo
+
+Reference: https://routley.io/posts/hand-writing-dns-messages/
+Details: function send_udp_message() was borrowed from the above site
+
+"""
 import argparse
 from sys import argv
 import socket
@@ -38,6 +45,7 @@ def extract_ans(hex_str):
     x = int(hex_str[12:16], 16)
     return x
 
+
 # Extract ip from hex when only one answer
 def extract_one_ip(hex_str):
     if hex_str == '':
@@ -53,6 +61,7 @@ def extract_one_ip(hex_str):
         ip += str(int(i, 16)) + '.'
     ip = ip.rstrip(ip[-1])
     return ip
+
 
 def send_udp_message(message, address, port):
     """send_udp_message sends a message to UDP server
@@ -70,31 +79,12 @@ def send_udp_message(message, address, port):
         sock.close()
     return binascii.hexlify(data).decode("utf-8")
 
-def format_hex(hex):
-    """format_hex returns a pretty version of a hex string"""
-    octets = [hex[i:i+2] for i in range(0, len(hex), 2)]
-    pairs = [" ".join(octets[i:i+2]) for i in range(0, len(octets), 2)]
-    return "\n".join(pairs)
 
 parser = argparse.ArgumentParser(description="""Server""")
-parser.add_argument('-f', type=str, help='File to read for root server', default='source_strings.txt', action='store', dest='in_file')
-parser.add_argument('port', type=int, help='This is the root server port to listen', action='store')
+parser.add_argument('port', type=int, help='This is server port to listen', action='store')
 # parser.add_argument('next_port', type=int, help='This is the top server port to listen', action='store')
 args = parser.parse_args(argv[1:])
 print(args)
-
-# load the text file with the ip addresses as dictionary
-domain_names = {}
-with open(args.in_file) as f:
-    for line in f:
-        key = line.lower().strip()
-        domain_names[key] = {'domain': key, 'ip': []}
-
-print(domain_names)
-print(domain_names['google.com']['domain'])
-
-# Find next server ip address
-thostname = ''
 
 
 # Create a new socket
@@ -129,41 +119,54 @@ while True:
 
     with csockid:
         while True:
+
+            # Read the request from client
             data = csockid.recv(512)
             data = data.decode('utf-8')
 
+            # Build message with UDP protocol format
             body = space_hex(hex_encode(data))
             message = header + " " + body + " " + end
+
+            # Request ip address from google DNS following UDP protocol
             response = send_udp_message(message, "8.8.8.8", 53)
-            print(response)
-            ans = extract_ans(response)
+            # print(response)
+
+            # Number of answer from google
+            num_ans = extract_ans(response)
 
             # No answer close connection
-            if ans == 0:
+            if num_ans == 0:
                 break
-            # Get the ip from the last
-            elif ans == 1:
+            # Get the ip from last bytes
+            elif num_ans == 1:
                 sent = extract_one_ip(response)
+                print('[S]: {}'.format(sent))
                 csockid.sendall(sent.encode('utf-8'))
             else:
-                csockid.sendall('121.121.121.121'.encode('utf-8'))
+                #
+                res_answer = response.split('c00c')
+                res_header = res_answer.pop(0)
 
+                # print(res_answer)
 
-            # try:
-            #     if domain_names[data] and domain_names[data][1] != 'NS':
-            #         print('[C]: {}'.format(data))
-            #         print('[S]: {}'.format(domain_names[data]))
-            #         csockid.sendall(str(domain_names[data][0] + ' ' + domain_names[data][1]).encode('utf-8'))
-            #     else:
-            #         csockid.sendall(str(thostname).encode('utf-8'))
-            #
-            #
-            # except:
-            #     if not data:
-            #         break
-            #     res_localhost = thostname
-            #     print('[C]: {}'.format(data))
-            #     print('[S]: {}'.format(res_localhost))
-            #     csockid.sendall(str(res_localhost).encode('utf-8'))
+                sent = ''
+
+                for i in res_answer:
+                    num = int(i[0:4], 16)
+
+                    # Check for type 1 and extract the ip
+                    if num == 1:
+                        sent += extract_one_ip(i) + ','
+                    else:
+                        # if not type 1 return the other and the ip
+                        sent += 'other' + ',' + extract_one_ip(i) + ','
+
+                # Remove extra character at the end
+                sent = sent.rstrip(sent[-1])
+                print('[S]: {}'.format(sent))
+
+                csockid.sendall(sent.encode('utf-8'))
+
 # ss.close()
 # exit()
